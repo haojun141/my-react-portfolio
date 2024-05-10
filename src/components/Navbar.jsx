bb7f784f53ed1401bbd41e8678fe406b61e7092e
haojun14 <haojun0114@outlook.com>
2024-05-10 17:32:54 +0800

    link to icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/J.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
@@ -8,12 +8,18 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="my logo" />
+        <img className="mx-2 w-20" src={logo} alt="my logo" />
       </div>
       <div className="m-8 flex item-center justify-center gap-4 text-2xl">
-        <FaGithub />
-        <FaInstagram />
-        <FaLinkedin />
+        <a href="https://github.com/haojun141">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/haojun.zhou/">
+          <FaInstagram />
+        </a>
+        <a href="www.linkedin.com/in/haojun-zhou-8ba39b186">
+          <FaLinkedin />
+        </a>
       </div>
     </nav>
   );
